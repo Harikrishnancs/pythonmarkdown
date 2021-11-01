43b19a54d4c53c0fe0ad25fe0abd2118674083e7
Harikrishnancs <49855474+Harikrishnancs@users.noreply.github.com>
2021-11-01 13:00:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import htmltabletomd
+from datetime import date
+today = date.today()
+d1 = today.strftime("%d-%m-%Y")
 with open('table.html','r') as file:
     countriesStr = file.read()
-#print(countriesStr)
-
 html_table = countriesStr
-
-
 md_table = htmltabletomd.convert_table(html_table)
 print(md_table)
 file = open("Python.md", "w")
+file.write("######**Success Report ("+d1+")**")
+file.write("\n")
 file.write(md_table)
 file.close()
